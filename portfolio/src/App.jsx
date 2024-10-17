434d3e517f9263615b6e22daa5525990838a6240
Carlos Berry <carlosberry@trentu.ca>
2024-10-17 14:17:29 -0400

    removed router

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import Routes from "./Routes";
 import Footer from "./Components/Footer";
+import Home from "./Pages/Home";
 
 function App() {
   return (
     <div className=" bg-slate-200 min-h-full flex flex-grow flex-col text-bl dark:text-white dark:bg-[#121619] tracking-wide">
-      <Routes />
+      <Home />
       <Footer />
     </div>
   );
